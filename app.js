f4157d1c99e593f205129d3507ba8677bc5beca4
DuongVietAnh-03 <vietkaiz2003@gmail.com>
2024-11-13 22:03:34 +0700

    T tao file 1

---
+++ app.js
@@ -0,0 +1,29 @@
+function calculate() {
+    var num1 = parseFloat(document.getElementById('num1').value);
+    var num2 = parseFloat(document.getElementById('num2').value);
+    var operation = document.getElementById('operation').value;
+    var result;
+
+    switch(operation) {
+        case "plus":
+            result = num1 + num2;
+            break;
+        case "subtract":
+            result = num1 - num2;
+            break;
+        case "multiply":
+            result = num1 * num2;
+            break;
+        case "divide":
+            if (num2 !== 0) {
+                result = num1 / num2;
+            } else {
+                result = "Cannot divide by zero";
+            }
+            break;
+        default:
+            result = "Invalid operation";
+    }
+
+    document.getElementById('result').innerText = "Result: " + result;
+}
